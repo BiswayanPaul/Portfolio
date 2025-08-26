2d152c6b7b652bf08f3681c73f4d6c9699fee9a0
Biswayan Paul <biswayanpaul2015@gmail.com>
2025-08-26 17:10:04 +0530

    Implement code changes to enhance functionality and improve performance

--- src/Data/Projects.tsx
+++ src/Data/Projects.tsx
@@ -1,9 +1,3 @@
-// data/projects.ts
-import DrawAPP from '../../public/drawapp.jpeg'
-import Blinkit from '../../public/blinkit.png'
-import PizzaSales from '../../public/pizzasales.png'
-import Healp from '../../public/healp.jpg' // Make sure you have this image in /public
-
 export interface Project {
     title: string
     description: string
@@ -16,27 +10,28 @@ export const projectList: Project[] = [
     {
         title: 'Healp - Healthcare Booking App',
         description: 'A modern full-stack platform to find doctors and hospitals, with appointment booking and dark mode support.',
-        image: Healp,
+        image: '/healp.jpg',
         github: 'https://github.com/BiswayanPaul/Healp',
         live: 'https://healp.vercel.app/',
     },
     {
+        title: 'Green Cart - Grocery Platform',
+        description: 'A MERN-stack e-commerce application for seamless grocery shopping with cart management and smooth checkout experience.',
+        image: '/greencart.jpg',
+        github: 'https://github.com/BiswayanPaul/PurpleMeritAssessment',
+        live: 'https://purple-merit-assessment-bay.vercel.app/',
+    },
+    {
         title: 'RealTime Collaborative Drawing App',
         description: 'Collaborate in real-time on a shared canvas.',
-        image: DrawAPP,
+        image: '/drawapp.jpeg',
         github: 'https://github.com/BiswayanPaul/drawapp-Frontend',
         live: 'https://drawapp-frontend.vercel.app/',
     },
     {
         title: 'BlinkIt Backend Project',
         description: 'Node.js/Express backend for an e-commerce platform.',
-        image: Blinkit,
+        image: '/blinkit.png',
         github: 'https://github.com/BiswayanPaul/BlinkIt-Backend-Xempla',
-    },
-    {
-        title: 'Pizza Sales Report',
-        description: 'Analyze pizza sales data with PowerBI.',
-        image: PizzaSales,
-        github: 'https://github.com/BiswayanPaul/Pizza-Sales-Analysis-Report',
     },
 ]
